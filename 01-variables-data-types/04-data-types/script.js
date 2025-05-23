e85e9235a0c753409f9d6367ae0b77993fa4a4b4
Ben Redmond <73973708+BenRedmondgithub@users.noreply.github.com>
2025-05-23 11:18:27 +0100

    update

---
+++ 01-variables-data-types/04-data-types/script.js
@@ -0,0 +1,29 @@
+// String
+
+let firstName = "Sara"; 
+
+// Number
+let age = 25;
+let height = 5.5;
+
+
+// Boolean
+let isStudent = true;
+let isEmployed = false;
+
+
+// null
+let emptyValue = null;
+
+// undefined 
+let notAssigned;
+
+// Symbol
+let uniqueId = Symbol("id");
+
+// BigInt
+let bigNumber = BigInt(1234567890123456789012345678901234567890);
+
+const output = uniqueId;
+
+console.log(output, typeof output); // null object
